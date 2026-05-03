c9bc5605ecfce167da24f0359ee3811edd6e1a20
hahyeonwon-sketch <hahyeonwon@gmail.com>
2026-05-04 02:33:41 +0900

    feat: 홈화면 구현 및 예외처리 개선
    - home.html 홈화면 구현 (배너, 네브바, 서비스 카드)
    - banner.png 배너 이미지 추가
    - HomeController / 및 /home 경로 추가
    - index.html 삭제 (대시보드 담당자 파일 제거)
    - ParkingException 적용 전체 Service 예외처리 통일
    - AdminParkingLogResponse 입주자 정산내역 무료 처리 수정
    - ParkingHistoryResponse 입주자 정산내역 무료 처리 수정
    - 알림 연동 (StaffEntryService, StaffExitService, VehicleServiceImpl)

--- src/main/java/com/hometalk/onepass/dashboard/controller/HomeController.java
+++ src/main/java/com/hometalk/onepass/dashboard/controller/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
     @GetMapping("/home")
     public String home() {
         return "home";
