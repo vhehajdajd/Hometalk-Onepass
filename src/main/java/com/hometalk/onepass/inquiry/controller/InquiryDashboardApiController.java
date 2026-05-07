fac7013a4b5fd9a4cda1a589ab8009b302727fab
Jaesoon33 <tnswoghkd2638@gmail.com>
2026-05-07 18:23:12 +0900

    [dev] merge 0507

--- src/main/java/com/hometalk/onepass/inquiry/controller/InquiryDashboardApiController.java
+++ src/main/java/com/hometalk/onepass/inquiry/controller/InquiryDashboardApiController.java
@@ -1,3 +1,4 @@
+/*
 package com.hometalk.onepass.admin.controller;
 
 import com.hometalk.onepass.inquiry.dto.InquiryDto;
@@ -20,4 +21,4 @@ public class AdminInquiryApiController {
     public ResponseEntity<List<InquiryDto>> recent(Authentication authentication) {
         return ResponseEntity.ok(inquiryService.findAdminRecent(authentication));
     }
-}
+}*/
