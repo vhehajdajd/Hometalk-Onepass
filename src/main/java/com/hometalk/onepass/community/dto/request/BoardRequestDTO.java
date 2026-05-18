47052422808ca5cdb042ea7d3c9deb993a15019c
yeons416 <yeonsmedi@gmail.com>
2026-05-18 16:15:36 +0900

    [community] enforce category code regex on detail admin page

--- src/main/java/com/hometalk/onepass/community/dto/request/BoardRequestDTO.java
+++ src/main/java/com/hometalk/onepass/community/dto/request/BoardRequestDTO.java
@@ -2,6 +2,7 @@ package com.hometalk.onepass.community.dto.request;
 
 import com.hometalk.onepass.community.entity.Board;
 import com.hometalk.onepass.community.enums.BoardType;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,10 @@ import lombok.Setter;
 public class BoardRequestDTO {
     private Long id;        // 수정 목적
     private String name;
+    @Pattern(
+            regexp = "^[a-zA-Z0-9_-]+$",
+            message = "게시판 영문 코드는 영어, 숫자, -, _ 만 가능합니다."
+    )
     private String code;
     private BoardType boardType;
 
